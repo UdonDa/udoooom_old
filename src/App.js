67818280d5e1bcdb2ade98e369cc1f4b31aaf8db
UdonDa <udoooon0727@gmail.com>
2018-05-10 00:04:42 +0900

    [Add]button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import RaisedButton from "./buttons/raisedButton";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <RaisedButton />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title"> Welcome to React </h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload
         </p>
       </div>
     );
---
+++ src/buttons/raisedButton.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "material-ui/styles";
+import Button from "material-ui/Button";
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit
+  },
+  input: {
+    display: "none"
+  }
+});
+
+function RaisedButtons(props) {
+  const { classes } = props;
+  return (
+    <div>
+      <Button variant="raised" className={classes.button}>
+        Default
+      </Button>
+    </div>
+  );
+}
+
+RaisedButtons.PropTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(RaisedButtons);
