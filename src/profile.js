b45df4b7d96996e140500c63758bc6e6346cfd08
UdonDa <udoooon0727@gmail.com>
2018-05-11 00:48:43 +0900

    [Add]twitter, facebook

--- src/profile.js
+++ src/profile.js
@@ -5,6 +5,7 @@ import Avatar from "material-ui/Avatar";
 import AccountIcon from "@material-ui/icons/AccountCircle";
 import SchoolIcon from "@material-ui/icons/School";
 import EmailIcon from "@material-ui/icons/Email";
+import BirthdayIcon from "@material-ui/icons/Cake";
 
 const styles = {
   container: {
@@ -29,32 +30,21 @@ function Profile(props) {
         <Avatar className={classes.icon}>
           <AccountIcon />
         </Avatar>
-        <div className={classes.text}>
-          うどん(@udoooom,{" "}
-          <a
-            href="https://twitter.com/udoooom/"
-            target="_blank"
-            className={classes.text}
-          >
-            Twitter
-          </a>)
-        </div>
+        <div className={classes.text}>うどん</div>
       </div>
 
       <div className={classes.container}>
         <Avatar className={classes.icon}>
           <AccountIcon />
         </Avatar>
-        <div className={classes.text}>
-          堀田 大地(Daichi Horita,
-          <a
-            href="https://www.facebook.com/daichi0727"
-            target="_blank"
-            className={classes.text}
-          >
-            Facebook
-          </a>)
-        </div>
+        <div className={classes.text}>堀田 大地 (Daichi Horita)</div>
+      </div>
+
+      <div className={classes.container}>
+        <Avatar className={classes.icon}>
+          <BirthdayIcon />
+        </Avatar>
+        <div className={classes.text}>1997/7/27</div>
       </div>
 
       <div className={classes.container}>
@@ -79,6 +69,32 @@ function Profile(props) {
         <a href="mailto:udooon0727@gmail.com" className={classes.text}>
           udooon0727@gmail.com
         </a>
+      </div>
+
+      <div className={classes.container}>
+        <Avatar src="/static/images/twitter.png" className={classes.icon} />
+        <div className={classes.text}>
+          <a
+            href="https://twitter.com/udoooom/"
+            target="_blank"
+            className={classes.text}
+          >
+            @udoooom
+          </a>
+        </div>
+      </div>
+
+      <div className={classes.container}>
+        <Avatar src="/static/images/facebook.png" className={classes.icon} />
+        <div className={classes.text}>
+          <a
+            href="https://www.facebook.com/daichi0727"
+            target="_blank"
+            className={classes.text}
+          >
+            @daichi0727
+          </a>
+        </div>
       </div>
     </div>
   );
