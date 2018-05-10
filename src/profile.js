81339874af9b4f5d76641afa054d1bcc3f4080d4
UdonDa <udoooon0727@gmail.com>
2018-05-10 23:56:11 +0900

    [Add]material-ui in profile

--- src/profile.js
+++ src/profile.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "material-ui/styles";
+import Avatar from "material-ui/Avatar";
+import SchoolIcon from "@material-ui/icons/School";
 
-class Profile extends React.Component {
-  render() {
-    return <div>Profile</div>;
-  }
+const styles = {
+  school: {}
+};
+function Profile(props) {
+  const { classes } = props;
+
+  return (
+    <div>
+      <Avatar className={classes.school}>
+        <SchoolIcon />
+      </Avatar>
+    </div>
+  );
 }
 
-export default Profile;
+Profile.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Profile);
