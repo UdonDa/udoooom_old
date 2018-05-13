01a520442b4b481080fe7e174ab1695f31399f81
UdonDa <udoooon0727@gmail.com>
2018-05-13 11:49:04 +0900

    [Add]activityの雛形

--- src/acitivity.js
+++ src/acitivity.js
@@ -1,9 +1,90 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "material-ui/styles";
+import classnames from "classnames";
+import Card, {
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions
+} from "material-ui/Card";
+import Collapse from "material-ui/transitions/Collapse";
+import Avatar from "material-ui/Avatar";
+import IconButton from "material-ui/IconButton";
+import Typography from "material-ui/Typography";
+import red from "material-ui/colors/red";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import HogeLogo from "./images/profile-logo.jpg";
 
-class Activities extends React.Component {
+const styles = {
+  school: {}
+};
+class Activities extends Component {
+  state = { expanded: false };
+  handleExpandClick = () => {
+    this.setState({ expanded: !this.state.expanded });
+  };
+
   render() {
-    return <div>activity</div>;
+    const { classes } = this.props;
+    return (
+      <div>
+        <Card className={classes.card}>
+          <CardHeader
+            avatar={
+              <Avatar aria-label="hackson" className={classes.avatar}>
+                R
+              </Avatar>
+            }
+            action={
+              <IconButton>
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title="hoge"
+            subheader="1997/7/7"
+          />
+          <CardMedia className={classes.media} image={HogeLogo} title="fuge" />
+          <CardContent>
+            <Typography component="p">くぼたみゆ〜〜〜〜</Typography>
+          </CardContent>
+          <CardActions className={classes.actions} disableActionSpacing>
+            <IconButton aria-label="Add to fav">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="share">
+              <ShareIcon />
+            </IconButton>
+            <IconButton
+              className={classnames(classes.expand, {
+                [classes.expandedOpen]: this.state.expanded
+              })}
+              onClick={this.handleExpandClick}
+              aria-expanded={this.state.expanded}
+              aria-label="Show More"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+          <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph variant="body2">
+                Hackson
+              </Typography>
+              <Typography>内容だよーん</Typography>
+            </CardContent>
+          </Collapse>
+        </Card>
+      </div>
+    );
   }
 }
 
-export default Activities;
+Activities.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Activities);
