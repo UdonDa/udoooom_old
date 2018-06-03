112a8b95b34673d36acf9971310b3b8daa972bde
UdonDa <udoooon0727@gmail.com>
2018-06-03 10:59:09 +0900

    [Add]/

--- src/home.js
+++ src/home.js
@@ -2,7 +2,11 @@ import React from "react";
 
 class Home extends React.Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        Welcome to udoooom
+      </div>
+    );
   }
 }
 
