a90e7d8d648d288f5b156f91b05aa9f2c7164ad9
UdonDa <udoooon0727@gmail.com>
2018-06-03 11:14:32 +0900

    [Fix]activity

--- src/home.js
+++ src/home.js
@@ -4,7 +4,8 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        Welcome to udoooom
+        <div>Welcome to udoooom</div>
+        <div>Please check Profile, Activity and Work.</div>
       </div>
     );
   }
